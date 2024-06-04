17d759d49f3568e63448a86ca906e76a92df1e00
DuyCR7 <anhduy0317@gmail.com>
2024-06-04 23:57:55 +0700

    Read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,15 +1,27 @@
 const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
     // [GET] /
-    async index(req, res) {
+    async index(req, res, next) {
+
+        // try {
+        //     const courses = await Course.find({});
+        //     res.json(courses);
+        // } catch (err) {
+        //     res.status(400).json({error: "Lỗi!!!"})
+        // }
+
+        // Promise
+        await Course.find({})
+            .then(courses => {
+                res.render('home', {
+                    courses: multipleMongooseToObject(courses) // convert lại
+                });
+            })
+            .catch(error => next(error));
+
         // res.render('home');
-        try {
-            const courses = await Course.find({});
-            res.json(courses);
-        } catch (err) {
-            res.status(400).json({error: "Lỗi!!!"})
-        }
     }
 
     // [GET] /search
