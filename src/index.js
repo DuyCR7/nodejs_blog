40c5c34ff50b2f95a3e146e74b9e199dc4204f56
DuyCR7 <anhduy0317@gmail.com>
2024-06-04 10:53:45 +0700

    Install, Prettier, Lint-staged Husky

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-const path = require('path');
+        const path = require('path');
 const { fileURLToPath } = require('url');
 const express = require('express'); // require tên thư viện (đi vào nodemodules)
-const morgan = require('morgan');
+                const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 
-const route = require('./routes/index'); 
+const route = require('./routes/index');
 
 const app = express();
 const port = 3000;
 
-app.use(express.urlencoded({
-    extended: true
-})); // middleware xử lý dữ liệu từ form
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+); // middleware xử lý dữ liệu từ form
 app.use(express.json()); // middleware xử lý dữ liệu từ javascript
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -20,13 +22,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('combined'));
 
 // Template engine
-app.engine('.hbs', engine(
-    {extname: '.hbs'}
-));
+app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', 'src/resources/views');
 
 // Routes init
 route(app);
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () =>
+    console.log(`Example app listening at http://localhost:${port}`),
+);
