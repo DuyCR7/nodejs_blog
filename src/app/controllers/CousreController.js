1fe71dc4e6d1858144999f847319258a86b3fe13
DuyCR7 <anhduy0317@gmail.com>
2024-06-06 16:31:30 +0700

    Create Course

--- src/app/controllers/CousreController.js
+++ src/app/controllers/CousreController.js
@@ -12,6 +12,26 @@ class CousreController {
             .catch(next);
         // res.send('show: ' + req.params.slug); // định nghĩa là gì thì phải viết giống
     }
+
+    // [GET] /courses/create
+    create(req, res, next) {
+        res.render('courses/create');
+    }
+
+    // [POST] /courses/store
+    async store(req, res, next) {
+        try {
+            const formData = req.body;
+            formData.image = 'https://files.fullstack.edu.vn/f8-prod/courses/6.png';
+            const course = new Course(formData);
+            await course.save();
+            // res.status(201).json({ message: 'Course created successfully', course });
+            res.redirect('/');
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ message: 'Server error, please try again later' });
+        }
+    }
 }
 
 module.exports = new CousreController();
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-updater');
+
+mongoose.plugin(slug);
 
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
-  name: {type: String, maxLength: 255},
-  description: {type: String},
+  name: {type: String, maxLength: 255, required: true},
+  description: {type: String, required: true},
   image: {type: String},
-  createdAt: {type: Date, default: Date.now},
-  updatedAt: {type: Date, default: Date.now}
+  slug: { type: String, slug: "name", unique: true},
+}, {
+  timestamps: true,
 });
 
 module.exports = mongoose.model('Course', Course);
