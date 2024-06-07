be7b9ac2c2bbcdca124770b27070aaba8a4bf127
DuyCR7 <anhduy0317@gmail.com>
2024-06-07 17:01:44 +0700

    Update Soft Delete Course

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -5,11 +5,30 @@ class MeController {
 
     // [GET] /me/stored/courses
     storedCourses(req, res, next) {
-        Course.find({})
-            .then(function(courses){
-                res.render('me/stored-courses', {courses: multipleMongooseToObject(courses)});
+
+        Promise.all([
+            Course.find({}),
+            Course.countDocumentsWithDeleted({ deleted: true })
+        ])
+        .then(([courses, deletedCount]) => {
+            res.render('me/stored-courses', {
+                deletedCount: deletedCount,
+                courses: multipleMongooseToObject(courses),
             })
-            .catch(next);
+        })
+        .catch(next);
+
+        // Course.countDocumentsWithDeleted({ deleted: true})
+        //     .then((deletedCount) => {
+        //         console.log(deletedCount);
+        //     })
+        //     .catch(() => {});
+
+        // Course.find({})
+        //     .then(function(courses){
+        //         res.render('me/stored-courses', {courses: multipleMongooseToObject(courses)});
+        //     })
+        //     .catch(next);
     }
 
     // [GET] /me/trash/courses
