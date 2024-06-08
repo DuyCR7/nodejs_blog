f7298bc1515ebcac37da8c53f0de7bd76ff4e31d
DuyCR7 <anhduy0317@gmail.com>
2024-06-08 10:07:53 +0700

    Autoincrement _id mongoose-sequence

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,17 +1,23 @@
 const mongoose = require('mongoose');
 const slug = require('mongoose-slug-updater');
 const mongooseDelete = require('mongoose-delete');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
+  _id: {type: Number},
   name: {type: String, maxLength: 255, required: true},
   description: {type: String, required: true},
   image: {type: String},
   slug: { type: String, slug: "name", unique: true},
 }, {
+  _id: false,
   timestamps: true,
 });
+
+// Autoincrement _id field
+Course.plugin(AutoIncrement);
 
 // Add plugins
 mongoose.plugin(slug);
